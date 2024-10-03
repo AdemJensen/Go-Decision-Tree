060347e91f94510e0cd9936960b3f1f4d792265c
Jensen <rentenglong@163.com>
2024-10-03 20:25:13 +0800

    fix: compile error

--- tree/prune_tree.go
+++ tree/prune_tree.go
@@ -2,6 +2,7 @@ package tree
 
 import "DecisionTree/config"
 
-func postPruneTree(conf config.Config, tree *Tree) error {
+func postPruneTree(conf *config.Config, tree *Tree) error {
+	// TODO: implement post prune tree
 	return nil
 }
